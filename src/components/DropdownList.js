d897d6083351f69d23dfdd931c2e7962d466de7e
abid <fmaabid@gmail.com>
2019-07-07 15:30:11 +0600

    add component for option

---
+++ src/components/DropdownList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+const DropdownList = (props) => (
+  <option>
+    {props.element}
+  </option>
+);
+
+export default DropdownList;
