9feb1fb452a6351263a7c36f0bb3700ee53d42b9
abid <fmaabid@gmail.com>
2019-07-07 15:29:38 +0600

    modify

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,18 @@
+import axios from "axios";
+
 const initialState = {
   amount: 0,
   resultHistory: []
 };
 
-const reducer = (state = initialState) => {
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'SLCT':
+      return {
+        ...state,
+        resultHistory: state.resultHistory
+      }
+  }
 
   return state;
 };
